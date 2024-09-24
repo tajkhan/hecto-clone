ea28af8fd2143963cd4fa337f08345703cca08d6
tajkhan <taj.muhammad.khan@gmail.com>
2024-09-24 14:35:11 +0500

    using crosterm events instead of stdin()

--- src/main.rs
+++ src/main.rs
@@ -23,28 +23,10 @@ use std::io::{self, Read};
 use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::disable_raw_mode;
 
+mod editor;
+use editor::Editor;
+
 fn main() {
-
-    enable_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                // let b = b.unwrap();
-                let c = b as char;
-                if c.is_control() {
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                }
-                else {
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                }
-                println!("{}", c);
-                if c == 'q' {
-                    disable_raw_mode().unwrap();
-                    break;
-                }
-            },
-            Err(err) => println!("Error: {}", err),
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
