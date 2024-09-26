09fa4f2942859440fde3f097f2d4faa9450f23d7
tajkhan <taj.muhammad.khan@gmail.com>
2024-09-26 11:14:30 +0500

    guarding against pothential arethmetic overflows

--- src/editor.rs
+++ src/editor.rs
@@ -68,9 +68,12 @@ impl Editor {
 
     fn draw_welcome_message() -> Result<(), Error> {
         let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
-        let width = Terminal::size()?.width as usize;
+        let width = Terminal::size()?.width;
         let len = welcome_message.len();
+
+        #[allow(clippy::integer_division)]
         let padding = (width + len) / 2;
+
         let spaces = " ".repeat(padding - 1);
         welcome_message = format!("~{spaces}{welcome_message}");
         welcome_message.truncate(width);
@@ -85,16 +88,18 @@ impl Editor {
 
     fn draw_rows() -> Result<(), Error> {
         let Size{height, ..} = Terminal::size()?; // returns incorrect height!!
-        let height = 55;    // fixing height
+        let height = 55 as usize;    // fixing height
         for current_row in 0..height {
             Terminal::clear_line()?;
+
+            #[allow(clippy::integer_division)]
             if current_row == height/3 {
                 Self::draw_welcome_message()?;
             } else {
                 Self::draw_empty_row()?;
             } 
 
-            if current_row + 1 < height {
+            if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
--- src/editor/term.rs
+++ src/editor/term.rs
@@ -7,17 +7,26 @@ use core::fmt::Display;
 
 #[derive(Debug, Copy, Clone)]
 pub struct Size {
-    pub height: u16,
-    pub width: u16,
+    pub height: usize,
+    pub width: usize,
 }
 
 #[derive(Copy, Clone)]
 pub struct Position {
-    pub x: u16,
-    pub y: u16,
+    pub x: usize,
+    pub y: usize,
 }
 
+
+/// Represents the Terminal.
+/// Edge Case for platforms where `usize` < `u16`:
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
+/// And should you attempt to set the cursor out of these bounds, it will also be truncated.
 pub struct Terminal {}
+
+
 
 impl Terminal {
 
@@ -55,13 +64,30 @@ impl Terminal {
         Ok(())
     }
 
+    /// Moves the cursor to the given Position.
+    /// # Arguments
+    /// * `Position` - the  `Position`to move the cursor to. Will be truncated to `u16::MAX` if bigger.
     pub fn move_cursor_to(pos: Position) -> Result<(), Error> {
-       Self::queue_command(MoveTo(pos.x, pos.y))?;
+
+       #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+       Self::queue_command(MoveTo(pos.x as u16, pos.y as u16))?;
+
         Ok(())
     }
 
+
+    /// Returns the current size of this Terminal.
+    /// Edge Case for systems with `usize` < `u16`:
+    /// * A `Size` representing the terminal size. Any coordinate `z` truncated to `usize` if `usize` < `z` < `u16`
     pub fn size() -> Result<Size, Error> {
-        let (height, width) = size()?;
+        let (height_u16, width_u16) = size()?;
+
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+
+        #[allow(clippy::as_conversions)]
+        let width = width_u16 as usize;
+
         Ok(Size{height, width})
     }
 
