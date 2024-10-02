92aa57dc843d8ba6feaf121c353c88257899041c
tajkhan <taj.muhammad.khan@gmail.com>
2024-10-02 16:05:25 +0500

    assignment 12 solution

--- src/editor/view/line.rs
+++ src/editor/view/line.rs
@@ -1,5 +1,7 @@
 use std::{cmp, ops::Range};
 
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct Line {
     string: String,
 }
@@ -14,10 +16,14 @@ impl Line {
     pub fn get(&self, range: Range<usize>) -> String {
         let start = range.start;    // will start always < string.len()?
         let end = cmp::min(range.end, self.string.len());
-        self.string.get(start..end).unwrap_or_default().to_string()
+        self.string
+            .graphemes(true)
+            .skip(start)
+            .take(end.saturating_sub(start))
+            .collect()
     }
 
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.string[..].graphemes(true).count()
     }
 }
